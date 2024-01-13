83a195f4d86fe86f4abb86655a37f6d049729730
Onur Baykal <theaob@gmail.com>
2024-01-13 17:42:01 +0300

    add rocket enemy collision logic

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,4 +6,7 @@ func _physics_process(delta):
 	global_position.x -= delta * speed
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
+	destroy()
+
+func destroy():
 	queue_free()
--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -7,3 +7,10 @@ func _physics_process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func _on_area_entered(area):
+	destroy()
+	area.destroy()
+
+func destroy():
+	queue_free()
