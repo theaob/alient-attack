80075d65b02a1e38fe83591f360f4df0a6284d3a
Onur Baykal <theaob@gmail.com>
2024-01-13 16:17:12 +0300

    removed unused script

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,30 @@
-extends Node2D
+extends CharacterBody2D
 
+const SPEED = 400
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(_delta):
+	
+	var rightPressed = Input.is_action_pressed("move_right")
+	var leftPressed = Input.is_action_pressed("move_left")
+	var upPressed = Input.is_action_pressed("move_up")
+	var downPressed = Input.is_action_pressed("move_down")
+	
+	var xVelocity = 0;
+	var yVelocity = 0;
+	
+	if rightPressed: xVelocity += SPEED
+	if leftPressed: xVelocity -= SPEED
+	if upPressed: yVelocity -= SPEED
+	if downPressed: yVelocity += SPEED
+	
+	velocity = Vector2(xVelocity,yVelocity)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	move_and_slide()
+	
+	var windowWidth = get_viewport_rect().size.x
+	var windowHeight = get_viewport_rect().size.y
+	
+	global_position.x = clampf(global_position.x, 0, windowWidth)
+	global_position.y = clampf(global_position.y, 0, windowHeight)
+	
 	pass
